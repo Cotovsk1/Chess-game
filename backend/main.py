f6d4f435f098d54db80f4f60dbeceddd0f12e812
Andrii <aistratij85@gmail.com>
2026-05-12 20:25:58 +0300

    minor fixes

--- backend/main.py
+++ backend/main.py
@@ -79,6 +79,12 @@ if os.path.isdir(frontend_path):
 def read_root():
     return {"message": "Шаховий сервер працює!"}
 
+from fastapi.responses import Response
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return Response(status_code=204)
+
 
 # Підключаємо наші роутери
 app.include_router(game.router, prefix="/game", tags=["game"])
